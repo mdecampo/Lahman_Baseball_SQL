4bd3acb1549098592b1e508ab2ac77b5e7b019a2
mdecampo <mdecampo@gmail.com>
2022-03-11 18:26:58 -0800

    finished through 10 corrected CTE

--- Scripts/baseball.sql
+++ Scripts/baseball.sql
@@ -183,8 +183,8 @@ Davey Johnson 1997 BAL and 2012 WAS, Jim Leyland 1988 PIT, 1990 PIT , 1992 PIT, 
 --10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 
 
-WITH CTE2 AS 
-(WITH CTE1 AS 
+/*WITH cte2 AS 
+(WITH cte1 AS 
 (select playerid,
 yearid,
 count(yearid) OVER(partition by playerid) AS years_played, 
@@ -196,12 +196,13 @@ group by playerid, yearid)
 Select playerid, yearid, year_hr,max_hr
 from CTE1
 where years_played>=10)
-select playerid, year_hr 
-from CTE2
+select playerid, year_hr, namefirst, namelast
+from people
+JOIN CTE2 using (playerid)
 where year_hr=max_hr
 AND yearid=2016 AND year_hr>=1
 
-
+A: 9 players, Robinson Cano (39), Bartolo Colon (1), Rajai Davis (12), Edwin Encarnaction (42,) Francisco Liriano (1), Mike Napoli (34), Angel Pagan (12), Justin Upton (31), Adam Wainwright (2)*/
 
 
 
